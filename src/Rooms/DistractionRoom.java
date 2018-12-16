43503be4c1a6c48bdea17a3e05eee2bdfa733d61
John Doe <johndoe@example.com>
2018-12-16 16:58:48 -0600

    Update src/Rooms/DistractionRoom.java

--- src/Rooms/DistractionRoom.java
+++ src/Rooms/DistractionRoom.java
@@ -10,11 +10,33 @@ public class DistractionRoom extends Room
     Person occupant;
     int xLoc,yLoc;
     String contains;
+	
+	String[] questions;
+	String[] answers;
+	int roomNumber;
 
     public DistractionRoom(int a, int b) {
         super(a, b);
         this.contains="[Eng]";
+		
+		questions = new String[/*numquestions*/];
+		answers = new String[/*numanswers*/];
+		roomNumber = /*num roms*/
+		
+		questions[0] = "Whats crakin";
+		answers[0]0 = "nah dawg";
     }
+	
+	
+	
+	public boolean checkAnswer(String userAnswer){
+		if(userAnswer.toLowerCase().equals(answers[roomNumber].toLowerCase())){
+			return true;
+		}
+		return false;
+	
+	}
+	
     public void enterRoom(Person x) {
 
         occupant = x;
