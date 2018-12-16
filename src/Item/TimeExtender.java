3ffb171f030bf784a0e6f6c0fe62739733ac7e10
John Doe <johndoe@example.com>
2018-12-16 16:42:16 -0600

    TImeExtender

--- src/Item/TimeExtender.java
+++ src/Item/TimeExtender.java
@@ -1,6 +1,18 @@
 package Item;
 
-//public class TimeExtender extends Item
+public class TimeExtender extends Item{
+	
+	
+	public TimeExtender(String ItemName, int xLoc, int yLoc){
+		super(String ItemName, int xLoc, int yLoc);
+	}
+	
+	public boolean checkPerson(int personX, int personY){
+		if(personX==getxLoc()&&personY == getyLoc()){
+			return true;
+		}return false;
+	}
+}
 //
 //public Item
 //}
